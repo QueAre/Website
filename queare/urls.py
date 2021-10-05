0caf449c427d8d90cd74109ec74b8a011e9cf9df
Foroutan <foroutan.dev@gmail.com>
2021-10-05 19:46:11 +0330

    Added Story Page

--- queare/urls.py
+++ queare/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,story
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('ourstory/', story)
 ]
--- queare/views.py
+++ queare/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, '../templates/index.html')
+
+def story(request):
+    return render(request, '../templates/story.html')
